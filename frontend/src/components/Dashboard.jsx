12f70086d45268286faa05099a056c5911dea817
dishagudup-2121 <dishagudup21@gmail.com>
2026-02-16 01:21:22 +0530

    add frontend folder

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -20,7 +20,8 @@ import "../App.css";
 import { getBrandTrend } from "../api/trendApi";
 
 
-const API = "http://127.0.0.1:8000";
+const API = import.meta.env.VITE_API_URL;
+
 
 export default function Dashboard() {
   const navigate = useNavigate();
