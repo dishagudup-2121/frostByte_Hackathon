26db5bd4f3fecc90a60ec1af607a7d477062ad17
dishagudup-2121 <dishagudup21@gmail.com>
2026-03-01 01:52:39 +0530

    Remove dotenv for Vercel runtime

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,6 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
